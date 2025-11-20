3704ed7ff3f13a1099275a7628f8bd5df38f848f
Python Developer Algebra Krunoslav Lujic <krunoslav.lujic@predavaci.algebra.hr>
2025-11-20 20:06:46 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,37 @@
 import requests
+# import json
 
 
 URL = 'https://dummyjson.com/recipes'
-PHOTO_URL = 'https://robohash.org/python1?set=set5'
 
 try:
-    response = requests.get(PHOTO_URL)
-    # response = requests.get(URL)
+    response = requests.get(URL)
     response.raise_for_status()
 
-    # content je binary -> odnosno file i koristi se za dohvat slika ili drugih datoteka
-    print(response.content)
-    with open('RoboHash.png', 'wb') as photo:
-        photo.write(response.content)
+    # .content je binary -> odnosno file i koristi se za dohvat slika ili drugih datoteka
+    # print(response.content)
+
+    # .text je str i korisitmo ga da bismo sadrzaj konvertirali u dict,
+    # odnosno objekt neke klase
+    # txt_data = response.text
+    # recipes_dict = json.loads(txt_data)
+    # print(recipes_dict)
+    # for recipe in recipes_dict['recipes']:
+    #     print()
+    #     print(recipe)
+    #     print()
+
+    # kraci nacin gore prikazanog nacina konverzije u dict:
+    print()
+    print('Nakon koristenja json() metode')
+    # .json() metoda je dio requests biblioteke i zato nije potrebno raditi
+    # import json standardnog modula
+    recipes = response.json()
+    for recipe in recipes['recipes']:
+        print()
+        print(recipe)
+        print()
+
 
 
 except Exception as ex:
